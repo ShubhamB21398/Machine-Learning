2d058d62c60beff380d0f6650e412a6a38926496
ShubhamB <shudhabankar@gmail.com>
2023-01-07 07:28:16 +0530

    Comments were added

--- Linear Regression/working.py
+++ Linear Regression/working.py
@@ -8,10 +8,12 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
 
+# creating a dataset
 X,y = datasets.make_regression(n_samples=100, n_features= 2, noise=20, random_state=4)
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=1234)
 
+# Class that we created manually
 from Linear_regression import linear_regression
 
 regressor = linear_regression(learning_rate = 0.01)
@@ -20,11 +22,13 @@ regressor.fit(X_train,y_train)
 
 predicted = regressor.predict(X_test)
 
+# To check the performance
 def mse(y_true,y_predicted):
     return np.mean((y_true-y_predicted)**2)
 
 print(mse(y_test , predicted))
 
+# Inbuilt linear regression model
 from sklearn.linear_model import LinearRegression
 
 regressor_py = LinearRegression()
